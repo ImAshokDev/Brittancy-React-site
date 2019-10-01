96c019b3236759ca75a3669d242cca6bdb910c5e
krist yohan <massashok420@gmail.com>
2019-10-01 16:46:05 +0530

    image added in slider section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import Header from './header/Header';
 import Menu from './menu/Menu';
+import Slider from './slider/Slider';
 
 const App = () => (
   <div className="brittancy">
     <Header h="110px" />
     <Menu />
+    <Slider />
   </div>
 );
 export default App;
