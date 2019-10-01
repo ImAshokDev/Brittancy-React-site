32dbf1634e09bde0c0f84d617c3351186231c72e
krist yohan <massashok420@gmail.com>
2019-10-01 18:47:01 +0530

    done with slider section

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -8,9 +8,9 @@ import img4 from './images/04.jpg';
 const Slider = () => (
   <div className="slider-section">
     <Grid image={img1} txt1="dark romantic" txt2="from " txt3="$50" />
-    <Grid image={img2} txt1="soul notes" txt2="from" txt3="$50" />
-    <Grid image={img3} txt1="orange mood" txt2="from" txt3="$50" />
-    <Grid image={img4} txt1="in the spotlight" txt2="from" txt3="$50" />
+    <Grid image={img2} txt1="soul notes" txt2="from " txt3="$50" />
+    <Grid image={img3} txt1="orange mood" txt2="from " txt3="$50" />
+    <Grid image={img4} txt1="in the spotlight" txt2="from " txt3="$50" />
   </div>
 );
 
@@ -21,9 +21,9 @@ const Grid = ({
     <div className="text">
       <h3>{txt1}</h3>
       <p>
-        {txt2} 
-{' '}
-<span>{txt3}</span>
+        {txt2}
+
+        <span>{txt3}</span>
       </p>
     </div>
   </div>
